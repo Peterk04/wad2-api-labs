54c8b4d638a742f40f488a47941f78933794a240
peterkeappock <peterkeappock@gmail.com>
2025-12-25 01:11:45 +0000

    added auth middleware

--- tasky-api/index.js
+++ tasky-api/index.js
@@ -2,11 +2,9 @@ import dotenv from 'dotenv';
 import express from 'express';
 import tasksRouter from './api/tasks';
 import './db';
-// other imports
 import cors from 'cors';
-//... other imports
 import usersRouter from './api/users';
-
+import authenticate from './authenticate';
 
 dotenv.config();
 
@@ -28,7 +26,7 @@ app.use(cors({
 
 app.use(express.json());
 
-app.use('/api/tasks', tasksRouter);
+app.use('/api/tasks', authenticate, tasksRouter);
 
 //Users router
 app.use('/api/users', usersRouter);
